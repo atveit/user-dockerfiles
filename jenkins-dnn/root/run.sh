2ed34d9958feeb187e0a888a63bf253fe6f90f29
Amund Tveit <amund@zedge.net>
2017-01-19 16:09:38 +0100

    added missing file

--- jenkins-dnn/root/run.sh
+++ jenkins-dnn/root/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nvidia-docker run -P --name jenkins-dnn -v /data/docker:/root/shared -d -it jenkins-dnn
+nvidia-docker run -P --name jenkins-dnn -v /data/docker:/root/shared -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 -p 50000:50000 -d -it jenkins-dnn
